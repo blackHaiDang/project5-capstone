89db274ab80fe5489af9a59bf096d309aac557b7
blackHaiDang <nguyenhaidang.simple@gmail.com>
2023-09-19 23:00:35 +0700

    change cluster name

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation deploy --template-file cloudformation/create-eks.yaml --stack-name capstone-project  --tags project=udapeople --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file cloudformation/create-eks.yaml --stack-name capstone-project5  --tags project=udapeople --capabilities CAPABILITY_NAMED_IAM
